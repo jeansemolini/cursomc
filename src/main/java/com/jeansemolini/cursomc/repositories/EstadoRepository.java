b788f9a9806dcd3a0b2cb8e8cd9b3b43a6e19259
Jean Semolini <jean.maiko@hotmail.com>
2018-09-26 20:56:33 -0300

    Estado e Cidade

---
+++ src/main/java/com/jeansemolini/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.jeansemolini.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jeansemolini.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>  {
+
+}
