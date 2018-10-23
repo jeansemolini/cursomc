b3d7d7e359e3b7bbde52de47fd70e78b15be5298
jeansemolini <jean.maiko@hotmail.com>
2018-10-23 19:52:03 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/jeansemolini/cursomc/resources/utils/URL.java
@@ -0,0 +1,32 @@
+package com.jeansemolini.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URL {
+	
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+	
+	public static List<Integer> decodeIntList(String s){
+		String[] vet = s.split(",");
+		List<Integer> list = new ArrayList<>();
+		for (int i=0; i < vet.length; i++) {
+			list.add(Integer.parseInt(vet[i]));
+		}		
+		return list;
+		
+		//converter vetor para lista -> Arrays.asList(s.split(",")).stream()
+		//para cada elemento da lista vai para o integer.parse -> map(x -> Integer.parseInt(x))
+		//devolver  lista -> collect(Collectors.toList())
+		//return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+	}
+
+}
