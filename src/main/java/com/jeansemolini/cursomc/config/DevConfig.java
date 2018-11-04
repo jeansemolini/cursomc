350c027882c74a279f8a64a63a044003b878eb83
jeansemolini <jean.maiko@hotmail.com>
2018-11-04 19:15:58 -0200

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/jeansemolini/cursomc/config/DevConfig.java
+++ src/main/java/com/jeansemolini/cursomc/config/DevConfig.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.jeansemolini.cursomc.services.DBService;
+import com.jeansemolini.cursomc.services.EmailService;
+import com.jeansemolini.cursomc.services.MockEmailService;
+import com.jeansemolini.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,6 +30,11 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
--- src/main/java/com/jeansemolini/cursomc/services/DBService.java
+++ src/main/java/com/jeansemolini/cursomc/services/DBService.java
@@ -117,7 +117,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "3636549621", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "jeansemolini@gmail.com", "3636549621", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("36216740","36092297"));
 
