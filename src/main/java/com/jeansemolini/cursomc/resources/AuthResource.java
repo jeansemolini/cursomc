5b2c1e3919b4b2d2680edffebca91831508f9ecc
jeansemolini <jean.maiko@hotmail.com>
2018-11-12 15:38:18 -0200

    Esqueci a senha

--- src/main/java/com/jeansemolini/cursomc/resources/AuthResource.java
+++ src/main/java/com/jeansemolini/cursomc/resources/AuthResource.java
@@ -1,17 +1,19 @@
 package com.jeansemolini.cursomc.resources;
 
-import java.util.Date;
-
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jeansemolini.cursomc.dto.EmailDTO;
 import com.jeansemolini.cursomc.security.JWTUtil;
 import com.jeansemolini.cursomc.security.UserSS;
+import com.jeansemolini.cursomc.services.AuthService;
 import com.jeansemolini.cursomc.services.UserService;
 
 @RestController
@@ -21,12 +23,21 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	@GetMapping("/refresh_token")
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
 		//response.setDateHeader("Expires", 1);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PostMapping("/forgot")
+	public ResponseEntity<Void> forgot(@RequestBody EmailDTO objDTO) {
+		service.sendNewPassword(objDTO.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 
--- src/main/java/com/jeansemolini/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/jeansemolini/cursomc/services/SmtpEmailService.java
@@ -9,6 +9,8 @@ import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
+import com.jeansemolini.cursomc.domain.Cliente;
+
 public class SmtpEmailService extends AbstractEmailService {
 	
 	@Autowired
@@ -31,6 +33,12 @@ public class SmtpEmailService extends AbstractEmailService {
 		LOG.info("Simulando envio de email...");
 		javaMailSender.send(msg);
 		LOG.info("Email enviado");
+	}
+
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
